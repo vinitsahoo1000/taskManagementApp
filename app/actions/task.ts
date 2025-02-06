553940952309afd00a7a5675c0c65fbc14cacf4b
vinitsahoo1000 <vinitsahoo1000@gmail.com>
2025-02-06 13:52:45 +0530

    setup nodejs runtime setup for vercel

--- app/actions/task.ts
+++ app/actions/task.ts
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 import { ActionResponse } from "./user";
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
+export const runtime = "nodejs";
 
 if (!JWT_SECRET) {
     throw new Error("JWT_SECRET is not defined in environment variables!");
--- app/actions/user.ts
+++ app/actions/user.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
+export const runtime = "nodejs";
 
 if (!JWT_SECRET) {
     throw new Error("JWT_SECRET is not defined in environment variables!");
