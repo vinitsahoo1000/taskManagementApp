9b689ae657731ca88f9cc00eecf1d8a583b18ee6
vinitsahoo1000 <vinitsahoo1000@gmail.com>
2025-02-07 00:41:09 +0530

    next config.ts and layout.tsx file edited

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-module.exports = {
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ["res.cloudinary.com"],
   },
-}
+};
 
 export default nextConfig;
